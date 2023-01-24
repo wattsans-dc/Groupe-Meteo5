b34e3c35798d2252e64c598729daf3ca8833d62b
RBREHAM <raphael.breham@viacesi.fr>
2023-01-24 14:54:07 +0100

    probleme cable esp regle

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
-import time
-import board
-import busio
-from adafruit_htu21d import HTU21D
-
-# Create library object using our Bus I2C port
-i2c = busio.I2C(board.SCL, board.SDA)
-sensor = HTU21D(i2c)
+from machine import *
+pin0 = Pin(0, Pin.OUT)
