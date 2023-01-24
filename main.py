047d34e895ffd717529b322ac4f0b08ad2b88ca3
RBREHAM <raphael.breham@viacesi.fr>
2023-01-24 13:45:42 +0100

    code_capteur

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-# main.py -- put your code here!
+import time
+import board
+import busio
+from adafruit_htu21d import HTU21D
+
+# Create library object using our Bus I2C port
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = HTU21D(i2c)
