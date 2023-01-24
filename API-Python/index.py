579f7b0567fe26e5609c1e82c588f0e581c184cf
wattsans <enzo.radici@viacesi.fr>
2023-01-24 15:36:57 +0100

    enzo commite API

---
+++ API-Python/index.py
@@ -0,0 +1,32 @@
+import mysql.connector
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+# Connexion à la base de données MySQL
+cnx = mysql.connector.connect(user='root', password='meteo', host='localhost', database='api')
+cursor = cnx.cursor()
+
+# Enregistrement des données de l'ESP
+@app.route('/data', methods=['POST'])
+def data():
+    data = request.get_json()
+    cursor.execute("INSERT INTO tablename (field1, field2, field3) VALUES (%s, %s, %s)", (data['field1'], data['field2'], data['field3']))
+    cnx.commit()
+    return 'Data received'
+
+# Renvoi des données en temps réel
+@app.route('/realtime', methods=['GET'])
+def realtime():
+    cursor.execute("SELECT * FROM tablename ORDER BY id DESC LIMIT 1")
+    result = cursor.fetchone()
+    return jsonify(result)
+
+# Renvoi des données historiques
+@app.route('/history', methods=['GET'])
+def history():
+    cursor.execute("SELECT * FROM tablename")
+    result = cursor.fetchall()
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
