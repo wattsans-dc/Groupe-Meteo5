72e677e7829d8f4a837fd0d9ab604318a3abc9b7
RBREHAM <raphael.breham@viacesi.fr>
2023-01-25 09:19:44 +0100

    Code du capteur en cours

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-from machine import *
-pin0 = Pin(0, Pin.OUT)
+import time
+import board
+import busio
+from adafruit_htu21d import HTU21D
+
+# Create library object using our Bus I2C port
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = HTU21D(i2c)
+
+print("\nTemperature: %0.1f C" % sensor.temperature)
