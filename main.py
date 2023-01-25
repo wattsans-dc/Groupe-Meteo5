1f17dac09fc4650f37b215ed6c34d08b27302110
RBREHAM <raphael.breham@viacesi.fr>
2023-01-25 13:57:55 +0100

    Bus I2c detecter (0x27 et 0x40)

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-import time
-import board
-import busio
-from adafruit_htu21d import HTU21D
+import machine, time
+i2c = machine.I2C(scl = machine.Pin(0), sda = machine.Pin(2), freq=400000)
 
-# Create library object using our Bus I2C port
-i2c = busio.I2C(board.SCL, board.SDA)
-sensor = HTU21D(i2c)
+while True:
 
-print("\nTemperature: %0.1f C" % sensor.temperature)
+    print(" Scanne du bus i2c… ")
+
+    i2c_peripheriques = i2c.scan()
+
+    if len(i2c_peripheriques) == 0:
+
+        print("Aucun périphériques i2c détectés !")
+
+    else:
+
+        print(len(i2c_peripheriques),  " périphériques i2c trouvés ")
+
+        for i2c_peripherique in i2c_peripheriques: 
+
+            print(hex(i2c_peripherique))
+
+    time.sleep(10)
+
