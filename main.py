b341e6526d3c2705b98d03dbc37ab20a2d37b70e
RBREHAM <raphael.breham@viacesi.fr>
2023-01-25 16:20:20 +0100

    clear le main

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("test groupe 5")
+import machine
+import sys 
+sys.path.append('D:\PROJETS\Projet CESI 01 - Météo\repo git\Groupe-M-t-o-5') 
+import sonde.py
+
+
+i2c = machine.I2C(scl = machine.Pin(0), sda = machine.Pin(2), freq=400000)
+s = SI7021(i2c)
+print(f"Temperature : {s.temperature()}")
+print(f"Humidite : {s.humidity()}")
