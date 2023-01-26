426bd0fa3ea398cd226a8df136e70ee33c1370f6
RBREHAM <raphael.breham@viacesi.fr>
2023-01-26 15:53:16 +0100

    Connexion BDD reussi

--- connexionbdd.py
+++ connexionbdd.py
@@ -1,11 +1,8 @@
 import requests
-import json
 
-url = 'http://192.168.137.187:5000/humidity'
+url = 'http://192.168.137.187:5000/data_from_sonde'
+data = {'degré': 69, 'teaux_humidité': 420}
 headers = {'Content-type': 'application/json'}
-data = {'teaux_humidité': 50}
-
-response = requests.post(url, data=json.dumps(data), headers=headers)
+response = requests.post(url, json=data, headers=headers)
 print(response.status_code)
 print(response.text)
-
