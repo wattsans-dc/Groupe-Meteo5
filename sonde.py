ead53508247c528aa1f828cf606e9deb521f37e4
RBREHAM <raphael.breham@viacesi.fr>
2023-01-26 13:50:27 +0100

    Code sonde optimisé

--- sonde.py
+++ sonde.py
@@ -23,20 +23,6 @@ def _crc(data):
 
 
 class SI7021:
-    """
-    A driver for the SI7021 temperature and humidity sensor.
- 
-    MicroPython example::
- 
-        import si7021
-        from machine import I2C, Pin
- 
-        i2c = I2C(-1, Pin(5), Pin(4))
-        s = si7021.SI7021(i2c)
-        print(s.temperature())
-        print(s.humidity())
- 
-    """
 
     def __init__(self, i2c, address=0x40):
         self.i2c = i2c
@@ -46,20 +32,14 @@ class SI7021:
 
     def init(self):
         self.reset()
-        # Make sure the USER1 settings are correct.
         while True:
-            # While restarting, the sensor doesn't respond to reads or writes.
             try:
                 value = self.i2c.readfrom_mem(self.address, _READ_USER1, 1)[0]
             except OSError as e:
-                if e.args[0] != 19:  # errno 19 ENODEV
+                if e.args[0] != 19:
                     raise
             else:
                 break
-
-    #        if value != _USER1_VAL:
-    #            raise RuntimeError("bad USER1 register (%x!=%x)" % (
-    #                value, _USER1_VAL))
 
     def _command(self, command):
         self.i2c.writeto(self.address, ustruct.pack('B', command))
@@ -68,14 +48,13 @@ class SI7021:
         data = bytearray(3)
         data[0] = 0xff
         while True:
-            # While busy, the sensor doesn't respond to reads.
             try:
                 self.i2c.readfrom_into(self.address, data)
             except OSError as e:
-                if e.args[0] != 19:  # errno 19 ENODEV
+                if e.args[0] != 19:
                     raise
             else:
-                if data[0] != 0xff:  # Check if read succeeded.
+                if data[0] != 0xff:
                     break
         value, checksum = ustruct.unpack('>HB', data)
         if checksum != _crc(data[:2]):
@@ -86,17 +65,6 @@ class SI7021:
         self._command(_RESET)
 
     def humidity(self, raw=False, block=True):
-        """
-        Start a humidity measurement.
- 
-        If ``block`` is ``True``, block until it is ready and return the
-        measured value. If it's ``False``, return None immediately, and the
-        value can be read later with a blocking call.
- 
-        If ``raw`` is ``True``, return the measured value as 16-bit integer,
-        otherwise convert it into percentage of relative humidity and return it
-        as a floating point number.
-        """
 
         if not self._measurement:
             self._command(_HUMID_NOHOLD)
@@ -112,17 +80,6 @@ class SI7021:
         return value * 125 / 65536 - 6
 
     def temperature(self, raw=False, block=True):
-        """
-        Start a temperature measurement.
- 
-        If ``block`` is ``True``, block until it is ready and return the
-        measured value. If it's ``False``, return None immediately, and the
-        value can be read later with a blocking call.
- 
-        If ``raw`` is ``True``, return the measured value as 16-bit integer,
-        otherwise convert it into Celsius degrees and return as a floating
-        point number.
-        """
         if not self._measurement:
             self._command(_TEMP_NOHOLD)
         elif self._measurement != _TEMP_NOHOLD:
@@ -146,4 +103,5 @@ if __name__ == "__main__":
     humid = s.humidity()
     print(f"Temperature : {temp}")
     print(f"Humidite : {humid}")
+
 
