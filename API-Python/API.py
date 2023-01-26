38dab1a13a6f3355fc2a39c3fa672a9fa8847795
wattsans <enzo.radici@viacesi.fr>
2023-01-26 17:01:58 +0100

    api

--- API-Python/API.py
+++ API-Python/API.py
@@ -1,4 +1,3 @@
-
 import pymysql
 from flask import Flask, request, jsonify
 app = Flask(__name__)
@@ -19,20 +18,20 @@ def data():
     data = request.get_json()
     cursor.execute("INSERT INTO meteo_donnée (degré) VALUES (%s)", (data['degré'],))
     cnx.commit()
-    return 'Données degré reçu'
+    return 'degré reçu'
 
 # Renvoi des données en temps réel
 @app.route('/realtime', methods=['GET'])
 def realtime():
     cnx = pymysql.connect(user='root', password='meteo', host='localhost', database='api')
     cursor = cnx.cursor()
-    cursor.execute("SELECT degré,teaux_humidité FROM meteo_donnée ORDER BY id DESC LIMIT 1")
+    cursor.execute("SELECT degre,teaux_humidite FROM meteo_donnée ORDER BY id DESC LIMIT 1")
     result = cursor.fetchone()
     cnx.close()
     print(result)
 #    socketio.emit('new_data', {"degré": result[0], "teaux_humidité": result[1]})
     #return jsonify({"degré": result[0], "teaux_humidité": result[1]})
-    return {"degré": result[0], "teaux_humidité": result[1]}
+    return {"degre": result[0], "teaux_humidite": result[1]}
 # Renvoi des données historiques
 @app.route('/history', methods=['GET'])
 def history():
@@ -46,7 +45,7 @@ def humidity():
     data = request.get_json()
     cursor.execute("INSERT INTO meteo_donnée (teaux_humidité) VALUES (%s)", (data['teaux_humidité'],))
     cnx.commit()
-    return 'Données humidité reçues'
+    return 'Humidity data received'
 
 #post donné capteur
 @app.route('/data_from_sonde', methods=['POST'])
@@ -54,16 +53,14 @@ def data_from_sonde():
     cnx = pymysql.connect(user='root', password='meteo', host='localhost', database='api')
     cursor = cnx.cursor()
     data = request.json
-    cursor.execute("INSERT INTO meteo_donnée (degré, teaux_humidité) VALUES (%s, %s)", (data['degré'], data['teaux_humidité']))
+    cursor.execute("INSERT INTO meteo_donnée (degre, teaux_humidite) VALUES (%s, %s)", (data['degre'], data['teaux_humidite']))
     cnx.commit()
     cursor.close()
     cnx.close()
-    return "Donées reçues et envoyées dans la base de données "
-
-    return "Correct"
+    return "Data received and added to the database"
+    return "kikoo"
 
 CORS(app, resources={r"/*": {"origins": "192.168.137.187"}})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
-
