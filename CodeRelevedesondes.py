c97966fb7d27c279531f94e434a38b16c109a2be
JBELIEN <jules.belien@viacesi.fr>
2023-01-26 09:37:16 +0100

    test bdd

--- CodeRelevedesondes.py
+++ CodeRelevedesondes.py
@@ -1,21 +1,39 @@
 import requests
 import mysql.connector
 
-cnx = mysql.connector.connect(user='nom_d_utilisateur', password='mot_de_passe',
-                              host='192.168.137.187',
-                              database='nom_de_la_bdd')
-
-
-moyenne = 0
-Valeur = 0
-Valeur1 = sonde.value
+"""MOYENNE DES 5 DERNIERES VALEURS POUR LA TEMPERATURE ET L'HUMIDITE"""
 i = 0
+ValeurTemp = 0
+ValeurHumid = 0
+moyenneTemp = 0
+moyenneHumid = 0
 
 while i < 4:
-    i =+ 1
-    Valeur = Valeur + Valeur1
-    time.sleep(2)
-    Valeur1 = sonde.value
-moyenne = Valeur / 5
+    ReceptionTemp = requests.get()
+    ReceptionHumid = requests.get()
+    ValeurTemp = ValeurTemp + ReceptionTemp
+    ValeurHumid = ValeurHumid + ReceptionHumid
+    i = i + 1
+moyenneTemp = ValeurTemp / 5
+moyenneHumid = ValeurHumid / 5
+
+
+"""IMPORT DANS LA BDD DE CES MOYENNES"""
+baseDeDonnees = mysql.connector.connect(user='root',
+                                        password='meteo',
+                                        host='192.168.137.187',
+                                        database='api')
+curseur = baseDeDonnees.cursor()
+curseur.execute("INSERT INTO api (temperature, humidite) VALUES (%s, %s)", (moyenneTemp, moyenneHumid))
+baseDeDonnees.commit()
+baseDeDonnees.close()
+
+
+
+
+
+
+
+
 
 
