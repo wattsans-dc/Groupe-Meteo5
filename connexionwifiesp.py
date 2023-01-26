470c13bc10f69a6b0e4f271852782f7529ff57e8
JBELIEN <jules.belien@viacesi.fr>
2023-01-26 11:03:54 +0100

    test bdd

---
+++ connexionwifiesp.py
@@ -0,0 +1,37 @@
+import network
+station = network.WLAN(network.STA_IF)
+station.active(True)
+
+
+def connect():
+    import network
+
+    ip = '192.168.1.10'
+    subnet = '255.255.255.0'
+    gateway = '192.168.1.1'
+    dns = '8.8.8.8'
+    ssid = "nomReseauWiFi"
+    password = "motDePasseWiFi"
+
+    station = network.WLAN(network.STA_IF)
+
+    if station.isconnected() == True:
+        print("Already connected")
+        return
+
+    station.active(True)
+    station.ifconfig((ip, subnet, gateway, dns))
+    station.connect()
+
+    while station.isconnected() == False:
+        pass
+
+    print("Connection successful")
+    print(station.ifconfig())
+
+
+def disconnect():
+    import network
+    station = network.WLAN(network.STA_IF)
+    station.disconnect()
+    station.active(False)
