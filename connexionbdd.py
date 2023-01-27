6dc36255626076667a879ed685a26997eca1073a
RBREHAM <raphael.breham@viacesi.fr>
2023-01-27 12:02:44 +0100

    petite correction

--- connexionbdd.py
+++ connexionbdd.py
@@ -1,7 +1,7 @@
 import requests
 
 url = 'http://192.168.137.187:5000/data_from_sonde'
-data = {'degré': 69, 'teaux_humidité': 420}
+data = {'degre': 69, 'teaux_humidite': 420}
 headers = {'Content-type': 'application/json'}
 response = requests.post(url, json=data, headers=headers)
 print(response.status_code)
